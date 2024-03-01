48fd5d73bc146492b6a3df2ee7b41681b52ca3dd
Danny Creations <usernamekepo@gmail.com>
2024-03-02 00:08:42 +0700

    feat(utilities): added waitForConnection

--- packages/utilities/src/helpers/request.helper.ts
+++ packages/utilities/src/helpers/request.helper.ts
@@ -1,12 +1,13 @@
 import got, { CancelableRequest, Options, Response } from 'got'
+import { sleep } from './sleep.helper'
 
 export * from 'got'
 export const request = got
 
-export async function requestTimeout<T>(options: Options & LongRunningRequestOptions) {
+export async function requestTimeout<T>(options: Options & TimeoutRequestOptions) {
 	const { initialTimeout = 10 * 1000, transmissionTimeout = 30 * 1000, totalTimeout = 60 * 1000 } = options
 
-	const instance = got(options) as CancelableRequest<Response<T>>
+	const instance = got({ retry: 0, ...options }) as CancelableRequest<Response<T>>
 
 	const cancel = () => instance.cancel()
 	const _totalTimeout = setTimeout(cancel, totalTimeout)
@@ -25,7 +26,32 @@ export async function requestTimeout<T>(options: Options & LongRunningRequestOpt
 	}
 }
 
-interface LongRunningRequestOptions {
+export function waitForConnection(options: Options & TimeoutRequestOptions = {}) {
+	return new Promise<boolean>((resolve) => {
+		const { totalTimeout = 10 * 1000 } = options
+
+		const wait = async () => {
+			try {
+				const res = await requestTimeout({
+					url: 'https://google.com',
+					totalTimeout,
+					...options,
+				})
+				if (res.statusCode === 200) {
+					return resolve(true)
+				}
+			} catch {
+				await sleep(totalTimeout)
+			}
+
+			return wait()
+		}
+
+		return wait()
+	})
+}
+
+interface TimeoutRequestOptions {
 	initialTimeout?: number
 	transmissionTimeout?: number
 	totalTimeout?: number
